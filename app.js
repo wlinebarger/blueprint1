01793c36224ad78886f850c21c6b0b8062fdd5a2
William Linebarger <william.linebarger@commercetools.com>
2018-06-15 14:43:07 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const Express = require('express')
+const Exphbs = require('express-handlebars');
+const App = Express()
+
+require('dotenv').config();
+
+const projectKey = process.env.PROJECT_KEY;
+/**
+ * Responds to any HTTP request that can provide a "message" field in the body.
+ *
+ * @param {!Object} req Cloud Function request context.
+ * @param {!Object} res Cloud Function response context.
+ */
+exports.helloWorld = (req, res) => {
+  // Example input: {"message": "Hello!"}
+  if (req.body.message === undefined) {
+    // This is an error case, as "message" is required.
+    res.status(400).send('No message defined!');
+  } else {
+    // Everything is okay.
+    console.log(req.body.message);
+    //res.status(200).send('Success: ' + req.body.message);
+    res.status(200).send('Success: ' + projectKey);
+  }
+};
