c289de4175beaa97c6f49d672e4690d3d3e5af54
William Linebarger <william.linebarger@commercetools.com>
2018-06-18 10:25:27 -0400

    test autodeploy

--- app.js
+++ app.js
@@ -15,11 +15,11 @@ exports.helloWorld = (req, res) => {
   // Example input: {"message": "Hello!"}
   if (req.body.message === undefined) {
     // This is an error case, as "message" is required.
-    res.status(400).send('No message defined!');
+    res.status(400).send('No message is defined!');
   } else {
     // Everything is okay.
     console.log(req.body.message);
     //res.status(200).send('Success: ' + req.body.message);
-    res.status(200).send('Success: ' + projectKey);
+    res.status(200).send('Successful: ' + projectKey);
   }
 };
